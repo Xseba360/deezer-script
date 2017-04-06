f6349b57401b40c53da34b3416133884667db6a1
Sebastian Gładki <sgladki1998@gmail.com>
2017-04-07 00:08:29 +0200

    work on theme

--- deeztron.js
+++ deeztron.js
@@ -1,3 +1,7 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
 var deeztron = {
     play: function () {
         var button = document.getElementsByClassName('control-play')[0];
@@ -16,7 +20,32 @@ var deeztron = {
         button.click();
     },
     settheme: function (theme) {
-        alert(theme);
+        if (theme == "dark") {
+            setdark();
+        } else {
+            setlight();
+        }
+
+        function setdark() {
+            var el = document.getElementById('myStyles');
+            if ( el !== null ) {
+            } else {
+                var oLink = document.createElement("link");
+                oLink.id   = 'myStyles';
+                oLink.href = "https://userstyles.org/styles/140761/dark-deezer.css";
+                oLink.rel = "stylesheet";
+                oLink.type = "text/css";
+                document.body.appendChild(oLink);
+            }
+        }
+        function setlight(){
+            var el = document.getElementById('myStyles');
+            if ( el !== null ) {
+                el.parentNode.removeChild(el);
+            } else {
+                
+            }
+        }
     },
     setvolume: function () {
 
