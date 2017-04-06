9b17a25cc00ec5a2592e1b8594c8040bd7c73a2a
Sebastian Gładki <sgladki1998@gmail.com>
2017-04-06 22:59:02 +0200

    init

---
+++ deeztron.js
@@ -0,0 +1,26 @@
+var deeztron = {
+    play: function () {
+        var button = document.getElementsByClassName('control-play')[0];
+        button.click();
+    },
+    prev: function () {
+        var button = document.getElementsByClassName('control-prev')[0];
+        button.click();
+    },
+    next: function () {
+        var button = document.getElementsByClassName('control-next')[0];
+        button.click();
+    },
+    stop: function () {
+        var button = document.getElementsByClassName('control-play')[0];
+        button.click();
+    },
+    settheme: function (theme) {
+
+    },
+    setvolume: function () {
+
+    }
+
+
+};
